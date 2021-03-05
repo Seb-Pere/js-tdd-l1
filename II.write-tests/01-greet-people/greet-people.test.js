54e287e271c5e57021a5a579bb78ddc98d469a13
Dylan <dds1991@hotmail.fr>
2021-03-05 08:32:15 +0100

    correction

--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -1,25 +1,20 @@
 const greetPeople = require("./greet-people");
 
-test("print list of names prefixed with Hello", function() {
-  const names = ['Irina', 'Ashleigh', 'Etza']
-  
-
-  const output = greetPeople(names)
+test("It should return a list of names prefixed with Hello", function() {
+  const output = greetPeople(['Irina', 'Ashleigh', 'Etza'])
   const expected = 'Hello IrinaAshleighEtza'
 
   expect(output).toEqual(expected)
 });
 
-test('print list of nanmes prefixed with Hello', function() {
-  const names = ['a', 'b', 'c']
-
-  const output = greetPeople(names)
+test('It should return a list of names prefixed with Hello', function() {
+  const output = greetPeople(['a', 'b', 'c'])
   const expected = 'Hello abc'
 
   expect(output).toEqual(expected)
 })
 
-test('It should throw an error if param is not an array', function() {
+test('It should throw a TypeError if the parameter is not an array', function() {
   expect(
     () => { greetPeople(42) }
   ).toThrowError(new TypeError('people.forEach is not a function'))
