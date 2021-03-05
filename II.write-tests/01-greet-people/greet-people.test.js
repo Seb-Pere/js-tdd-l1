29111211dde972283da61171e1e3dbaf6f1c0977
Dylan <dds1991@hotmail.fr>
2021-03-05 08:28:49 +0100

    correction

--- II.write-tests/01-greet-people/greet-people.test.js
+++ II.write-tests/01-greet-people/greet-people.test.js
@@ -1,5 +1,26 @@
+const greetPeople = require("./greet-people");
+
 test("print list of names prefixed with Hello", function() {
-  // Arrange
-  // Act
-  // Assert
+  const names = ['Irina', 'Ashleigh', 'Etza']
+  
+
+  const output = greetPeople(names)
+  const expected = 'Hello IrinaAshleighEtza'
+
+  expect(output).toEqual(expected)
 });
+
+test('print list of nanmes prefixed with Hello', function() {
+  const names = ['a', 'b', 'c']
+
+  const output = greetPeople(names)
+  const expected = 'Hello abc'
+
+  expect(output).toEqual(expected)
+})
+
+test('It should throw an error if param is not an array', function() {
+  expect(
+    () => { greetPeople(42) }
+  ).toThrowError(new TypeError('people.forEach is not a function'))
+})
